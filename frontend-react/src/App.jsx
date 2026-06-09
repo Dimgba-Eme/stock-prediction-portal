916b52696d59b381112ecaa57edb25cb4bc6c893
Dimgba Eme <dimgbaagwueme@gmail.com>
2026-06-09 13:32:38 +0100

    login/logout functionality

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -6,24 +6,26 @@ import Main from './components/Main'
 import Footer from './components/Footer'
 import Register from './components/Register'
 import Login from './components/Login'
+import AuthProvider from './AuthProvider'
 
 function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <Header />
-
-        <Routes>
-          <Route path='/' element={<Main />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
-        </Routes>
-
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Main />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </>
   )
 }
 
 export default App
+
